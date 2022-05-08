184a9da27106ae2dfe2fdf06f8d7040c6557fd2a
Richa Sondhi <richa.sondhi@ibm.com>
2022-05-08 21:56:14 +0200

    Product Difference Solution Fixed Features

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public void setC(int c) { this.c = c; }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getDifference() {
+        return a * b - c * d;
+    }
+}
